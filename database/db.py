e564b2682d713322db8a7eb14384fee32d8df17a
Bharat <brai65917@gmail.com>
2026-06-13 14:33:01 +0530

    deployment ready

--- database/db.py
+++ database/db.py
@@ -1,17 +1,43 @@
 import os
 import psycopg2
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
+def get_database_url():
+
+    # Streamlit Cloud
+    if "DATABASE_URL" in st.secrets:
+        print("using secrets env")
+        return st.secrets[
+            "DATABASE_URL"
+        ]
+
+    # Local .env
+    print("Using Local Environment")
+    return os.getenv(
+        "DATABASE_URL"
+    )
+
+
 def get_connection():
+
     try:
+
         conn = psycopg2.connect(
-            os.getenv("DATABASE_URL")
+            get_database_url(),
+            sslmode="require"
         )
+
         return conn
 
     except Exception as e:
-        print("Database connection failed:", e)
+
+        print(
+            "Database connection failed:",
+            e
+        )
+
         return None
